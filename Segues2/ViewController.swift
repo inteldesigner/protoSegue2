1e4781a6f8896deed89a9d369c675d15124b0696
Eric <ericstein.rl@gmail.com>
2019-04-26 17:36:44 -0500

    first comnmit

--- Segues2/ViewController.swift
+++ Segues2/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "goToSecondScreen", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToSecondScreen"{
+            let destinationVC = segue.destination as! SecondViewController
+            
+            destinationVC.textPassedOver = textField.text!
+        }
+    }
 }
 
